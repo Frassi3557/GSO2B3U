a4a8012bb37b9b77b7ba04a1350b9c8ed81c64c7
Snorri Freyr Ásgeirsson <freysi_freys@live.com>
2016-10-05 11:25:55 +0000

    Add files via upload

--- Verkefni-3/includes/footer.php
+++ Verkefni-3/includes/footer.php
@@ -12,4 +12,4 @@ else
 	echo "{$startYear}&ndash;{$thisYear}";
 } ?> Snorri Freyr Ásgeirsson</p>
 <p class="col-1-3"><a href="http://tsuts.tskoli.is/2t/0506992429">Efnisyfirlit</a></p>
-<p class="col-1-3"><a href="https://github.com/Frassi3557/GSO2B3U">GitHub</a></p>
+<p class="col-1-3"><a href="https://github.com/Frassi3557/GSO2B3U/tree/master/Verkefni-3">GitHub</a></p>
